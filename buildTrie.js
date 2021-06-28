56b63413c1ae5c53f866e52d4722b2733c0637cf
Javier A. Gamboa D <xaviz2007@hotmail.com>
2021-06-28 05:47:39 -0500

    Not working, problem misunderstood

--- buildTrie.js
+++ buildTrie.js
@@ -30,3 +30,52 @@ function buildTrie(...words) {
     }
 }
 
+
+// function buildTrie(...words) {
+//     var objectTrie = {}
+//     if (words.length === 0) {
+//         return objectTrie
+//     } else if (words.length === 1 && words == "") {
+//         return objectTrie
+//     } else {
+//         for (let i = 0; i < words.length; i++) {
+
+//             let currentWord = words[i];
+
+//             var objectCurrentWord = {};
+
+//             for (let j = currentWord.length; j > 0; j--) {
+//                 console.log(j)
+//                 var newWord = currentWord
+//                 console.log(currentWord.length)
+//                 if (j >= 1 && j < currentWord.length) {
+
+//                     newWord = currentWord.slice(0, j);
+//                   if(j!==1){
+//                     objectCurrentWord = { [newWord]:objectCurrentWord }
+//                   }
+
+//                 } else if (j === currentWord.length) {
+
+//                     objectCurrentWord = { [currentWord]: null }
+//                 }
+//                 console.log(objectCurrentWord)
+//             }
+//             if(i === 0 ) {
+//               if(objectCurrentWord[newWord] === null) {
+//                 objectTrie = objectCurrentWord
+//               } else {
+//                 objectTrie = objectCurrentWord
+//               }
+              
+//             }else{
+//               objectTrie[newWord] = objectCurrentWord
+//             }
+            
+//         }
+//         return objectTrie
+
+//     }
+// }
+
+
