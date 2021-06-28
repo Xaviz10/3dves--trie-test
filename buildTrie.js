505779d936811433cc36976256834994c4d22d82
Javier A. Gamboa D <xaviz2007@hotmail.com>
2021-06-28 01:11:59 -0500

    First 2 cases with if statement

---
+++ buildTrie.js
@@ -0,0 +1,9 @@
+function buildTrie(...words) {
+    console.log(words.length)
+    let objectTrie = {}
+    if (words.length === 0) {
+        return objectTrie
+    } else if (words.length === 1 && words == "") {
+        return objectTrie
+    }
+}
