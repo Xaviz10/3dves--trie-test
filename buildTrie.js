b894c9121212a6690e795af0b149f30def440d67
Javier A. Gamboa D <xaviz2007@hotmail.com>
2021-06-28 02:45:15 -0500

    Object of constructor build trie of each word, but return undefined to buildTrie

--- buildTrie.js
+++ buildTrie.js
@@ -1,9 +1,27 @@
 function buildTrie(...words) {
-    console.log(words.length)
     let objectTrie = {}
+    console.log('array', words)
     if (words.length === 0) {
         return objectTrie
     } else if (words.length === 1 && words == "") {
         return objectTrie
+    } else {
+        let a = words.map(item => constructorTrie(item, null))
+        console.log('a', a)
     }
 }
+
+const constructorTrie = (word, object) => {
+    console.log(object)
+
+    if (word.length >= 1) {
+        let newWord = word.slice(0, word.length - 1)
+        constructorTrie(newWord, { [word]: object })
+
+    } else {
+        console.log('final', object)
+        return object
+    }
+}
+
+
